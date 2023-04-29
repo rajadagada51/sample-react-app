378c73004a4162f3caef3a2081ad3cdf6bedb6ac
Ubuntu <Azure@python.uh01sy4q0cfufbs0dl0j30pdnh.bx.internal.cloudapp.net>
2023-04-29 06:44:25 +0000

    Added react Files

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('/')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>UI App</h1>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
